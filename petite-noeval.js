01ee815dc5cb6e164f607b9621441747fac48954
Weston <classicmclive@gmail.com>
2024-12-07 14:31:05 -0500

    Create petite-noeval.js

---
+++ petite-noeval.js
@@ -0,0 +1,17 @@
+function $(val){	
+	return val;
+}
+function $set(val){
+	setTimeout(()=>{$$render()},0);
+	return val;
+}
+let saved = [];
+function $scope(func,loc){
+	saved.push([func,document.getElementById(loc)]);
+  $$render();
+}
+function $$render(){
+	for(let i=0;i<saved.length;i++){
+  	saved[i][1].innerHTML = saved[i][0]()
+  }
+}
