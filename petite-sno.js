404c8a40eaaf4afa04c828a3453ae0bee8ffc0c6
Weston <classicmclive@gmail.com>
2024-12-07 14:42:35 -0500

    Update petite-sno.js

--- petite-sno.js
+++ petite-sno.js
@@ -2,17 +2,13 @@ let saved = [];
 function $scope(varName){
 	let elem = document.getElementById(varName);
   let update = `()=>{return ${varName};}`;
-  let func = eval(update);
-  elem.innerHTML = func();
-  saved.push([elem,func])
-}
-function $$render(){
-	for(let i=0;i<saved.length;i++){
-  	saved[i][0].innerHTML = saved[i][1]();
-  }
+  elem.innerHTML = eval(update)();
+  saved.push([elem,eval(update)])
 }
 
 function $(stuff){
 	eval(stuff);
-  $$render();
+  for(let i=0;i<saved.length;i++){
+  	saved[i][0].innerHTML = saved[i][1]();
+  }
 }
