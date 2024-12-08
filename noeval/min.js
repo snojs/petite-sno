c70daf4b7937cc2a3b6b9e1df9802dff801d86b5
Weston <classicmclive@gmail.com>
2024-12-08 17:11:47 -0500

    Update min.js

--- noeval/min.js
+++ noeval/min.js
@@ -1,1 +1,1 @@
-function $set(e){return setTimeout(()=>{$$render()},0),e}let saved=[];function $scope(e,n){saved.push([e,document.getElementById(n)]);for(let t=0;t<saved.length;t++)saved[t][1].innerHTML=saved[t][0]()}
+function $set(e){return setTimeout(()=>{$$render()},0),e}let saved=[];function $scope(e,n){saved.push([e,document.getElementById(n)]),$$render()}function $$render(){for(let e=0;e<saved.length;e++)saved[e][1].innerHTML=saved[e][0]()}
