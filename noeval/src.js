3b7a55ec7c687f00590ba2328e250bb76681d34e
Weston <classicmclive@gmail.com>
2024-12-08 17:11:27 -0500

    Update src.js

--- noeval/src.js
+++ noeval/src.js
@@ -5,6 +5,9 @@ function $set(val){
 let saved = [];
 function $scope(func,loc){
   saved.push([func,document.getElementById(loc)]);
+  $$render()
+}
+function $$render(){
   for(let i=0;i<saved.length;i++){
     saved[i][1].innerHTML = saved[i][0]()
   }
