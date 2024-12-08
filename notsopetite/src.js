d58a830594e3cf07b4e0a4693c1834a6234de4b6
Weston <classicmclive@gmail.com>
2024-12-07 22:59:03 -0500

    Create src.js

---
+++ notsopetite/src.js
@@ -0,0 +1,31 @@
+let bounds = []
+const rebound = () => {
+	for(let i=0;i<bounds.length;i++){
+  	rebind(bounds[i]);
+  }
+}
+const rebind = (what) =>{
+	let elem = document.getElementsByClassName(what);
+  for(let i=0;i<elem.length;i++){
+    for(let q=0;q<Object.keys(state).length;q++){
+      if(Object.keys(state)[q] == what){
+        elem[i].innerHTML = state[Object.keys(state)[q]]
+      }
+    }
+  }
+}
+let state = new Proxy({},{
+	set(target,property,value){
+    bounds.push(property)
+    target[property] = value;
+    rebound()
+    return true;
+  }
+})
+function forEach(array,template){
+  let final = "";
+  for(let i=0;i<array.length;i++){
+    final+=template.replaceAll("{{item}}",array[i]).replaceAll("{{i}}",i);
+  }
+  return final;
+}
