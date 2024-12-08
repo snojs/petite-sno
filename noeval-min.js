191d4d18bedf54847226fa0309f26e2e5e4a8208
Weston <classicmclive@gmail.com>
2024-12-07 22:53:27 -0500

    Delete noeval-min.js

--- noeval-min.js
+++
@@ -1,1 +0,0 @@
-function $set(e){return setTimeout(()=>{$$render()},0),e}let saved=[];function $scope(e,n){saved.push([e,document.getElementById(n)]);for(let t=0;t<saved.length;t++)saved[t][1].innerHTML=saved[t][0]()}
