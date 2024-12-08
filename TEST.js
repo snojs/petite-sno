eb518236739bade4b5a00cb348e6d1c76d1eb3ac
Weston <classicmclive@gmail.com>
2024-12-07 22:31:30 -0500

    Update TEST.js

--- TEST.js
+++ TEST.js
@@ -1,46 +1,24 @@
-const getValue = (updated) =>{
-	/* console.log(updated) */
-}
 let bounds = []
-const bind = (what,ifarr=undefined) =>{
-	bounds.push([what,ifarr])
-  rebound()
-}
 const rebound = () => {
 	for(let i=0;i<bounds.length;i++){
-  	rebind(bounds[i][0],bounds[i][1])
+  	rebind(bounds[i]);
   }
 }
-const rebind = (what,ifarr=undefined) =>{
-	let elem = document.getElementById(what);
- 	if(ifarr != undefined){
-  	for(let q=0;q<Object.keys(state).length;q++){
-    	if(Object.keys(state)[q] == what){
-      	elem.innerHTML = state[Object.keys(state)[q]][ifarr]
-      }
-    }
-  }
-  else{
+const rebind = (what) =>{
+	let elem = document.getElementsByClassName(what);
+  for(let i=0;i<elem.length;i++){
     for(let q=0;q<Object.keys(state).length;q++){
-    	if(Object.keys(state)[q] == what){
-      	elem.innerHTML = state[Object.keys(state)[q]]
+      if(Object.keys(state)[q] == what){
+        elem[i].innerHTML = state[Object.keys(state)[q]]
       }
     }
   }
 }
 let state = new Proxy({},{
 	set(target,property,value){
+    bounds.push(property)
     target[property] = value;
     rebound()
     return true;
   }
 })
-
-
-state.count = 0
-bind("count");
-function add(){
-	state.count++
-}
-
-/* for(let i=0;) */
